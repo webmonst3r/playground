76111e5fd57a331747fc7afbb0f4226a2e65a319
masterthepixel <45719243+masterthepixel@users.noreply.github.com>
2020-03-03 19:57:55 -0500

    Deploy

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./public/**/*.html'],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_.:/]+/g) || []
 })
 
 module.exports = {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
 module.exports = {
   theme: {
-    extend: {},
+    interFontFeatures: {
+      default: ['calt', 'liga', 'kern'],
+      numeric: ['tnum', 'salt', 'ss02']
+    },
+    extend: {
+      fontSize: {
+        'xs': '11px',
+        's': '13px',
+        'm': '17px',
+        'l': '28px',
+        'xl': '36px',
+        '2xl': '48px',
+        '3xl': '64px',
+        base: '16px',
+      },
+      lineHeight: {
+        'lg': '1.6',
+        'md': '1.33',
+        'sm': '1.25',
+        'xs': '1',
+      },
+    }
   },
-  variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/ui'),
+    require('tailwindcss-debug-screens'),
+    require('tailwindcss-font-inter')(),
+    require('@tailwindcss/ui')({
+      layout: 'sidebar',
+    })
+  ]
 }
